c27aacdaa2388ea0c2c60d4225a3eb97e4050992
renee.llup <renee.llup@gmail.com>
2020-07-19 10:47:45 +0800

    According to my connection mogodb

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 var logger = require("morgan"),
-cors = require("cors"),
-http = require("http"),
-express = require("express"),
-bodyParser = require("body-parser"),
-mongoose = require('mongoose');
+  cors = require("cors"),
+  http = require("http"),
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
 var app = express();
 var port = 3000;
-var userCtrl = require('./user-controller');
+var userCtrl = require("./user-controller");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-app.listen(port, function(err){
-    console.log("Listening on Port: " + port)
+app.listen(port, function (err) {
+  console.log("Listening on Port: " + port);
 });
-
-mongoose.connect('mongodb://localhost/test');
-mongoose.connection.on('error', (err) => { 
-    console.log('Mongodb Error: ', err); 
-    process.exit();
+//{user}:{password}@{host}:{port}/{database}
+mongodb: mongoose.connect(
+  "mongodb://admin:admin123@178.238.234.21:27017/testdb"
+);
+mongoose.connection.on("error", (err) => {
+  console.log("Mongodb Error: ", err);
+  process.exit();
 });
-mongoose.connection.on('connected', () => { 
-    console.log('MongoDB is successfully connected');
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB is successfully connected");
 });
